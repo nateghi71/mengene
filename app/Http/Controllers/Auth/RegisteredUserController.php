ed861da1d4a33c1881a8d453a8de591688c3701e
hoosein nateghi <hooseinnateghi7198@gmail.com>
2024-01-14 15:42:51 +0330

    uncomment sms

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -49,7 +49,7 @@ class RegisteredUserController extends Controller
         ]);
 
         $smsApi = new SmsAPI();
-//        $smsApi->sendSmsCode($userNumber , $code);
+        $smsApi->sendSmsCode($userNumber , $code);
 
         session()->put('randomString' , $randomString);
         return redirect()->route('2fa.enter_code');
